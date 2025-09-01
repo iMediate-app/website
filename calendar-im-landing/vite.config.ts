db8af369c6bf2af3d13c3fdead25a7b3121ed47a
iMediate-app <imediate.app@gmail.com>
2025-09-02 00:24:51 +0200

    fixed json

---
+++ calendar-im-landing/vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  // 👇 replace with your repo name
+  base: '/calendar-im-landing/',
+})
